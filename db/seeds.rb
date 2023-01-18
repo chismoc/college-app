ff893e1a8a9dd5c59af5568414ddeca66f0fc41a
chismoc <chisomo.chingwalu@yahoo.co.uk>
2023-01-18 16:18:12 +0200

    add student seed data and display in view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+student = Student.create(
+    first_name: 'Jane',
+    last_name: 'Doe',
+    email: 'jane@imc.com',
+    address: '1 Doe street',
+    dob: '2000,11,12'
+   )
+
+   20.times do |i|
+    puts "Creating student #{i+1}"
+    Student.create(
+        first_name: "Student #{i+1}",
+        last_name: "Lname #{i+1}",
+        email: "student#{i+1}@imc.com",
+        address: "Doe street #{i+1}",
+        dob: '2000,11,12'
+    )
+   end
