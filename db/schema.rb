94be5eb0f63a5741001a7dd4805d1e9594e4265d
chismoc <chisomo.chingwalu@yahoo.co.uk>
2023-01-18 13:05:37 +0200

    add student model

---
+++ db/schema.rb
@@ -0,0 +1,11 @@
+
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_104715) do
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
