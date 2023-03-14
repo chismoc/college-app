78dc745ce0561242d6dcedeb6b903154fdea8c88
chismoc <chisomo.chingwalu@yahoo.co.uk>
2023-03-14 20:47:26 +0200

    add blog model and association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_124031) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_145855) do
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.text "description"
