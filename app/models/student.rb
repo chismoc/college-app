642ead919fae0e17929f3fbf2767b823e0035e62
chismoc <chisomo.chingwalu@yahoo.co.uk>
2023-03-14 21:22:55 +0200

    create project model and association

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
     has_many :blogs
+    has_many :student_projects
+    has_many :projects, through: :student_projects
     validates :first_name, :last_name, :email, presence: true
 end
