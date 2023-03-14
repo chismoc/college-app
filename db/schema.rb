41bb585acd70a0709ea8361f16212908ef91a443
chismoc <chisomo.chingwalu@yahoo.co.uk>
2023-03-14 22:22:39 +0200

    add submission date to student projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_191046) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_192404) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_191046) do
     t.integer "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "submission_date"
     t.index ["project_id"], name: "index_student_projects_on_project_id"
     t.index ["student_id"], name: "index_student_projects_on_student_id"
   end
