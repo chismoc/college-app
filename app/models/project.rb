52705a71e5d6251202bc3301835fef07a3a5a208
chismoc <chisomo.chingwalu@yahoo.co.uk>
2023-03-17 12:41:23 +0200

    add active concern for course and project validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
-has_many :student_projects
-has_many :students, through: :student_projects
+    include Validatable
+    has_many :student_projects
+    has_many :students, through: :student_projects
 end
